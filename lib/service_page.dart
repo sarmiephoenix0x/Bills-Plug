420e2d0cdd14a68dcf449bbb0f30fa235d47a617
PhilipNzube <Onwubaliliphilip@gmail.com>
2024-10-28 10:29:43 +0100

    Made Changes (28/10/2024)

--- lib/service_page.dart
+++ lib/service_page.dart
@@ -19,94 +19,183 @@ class _ServicePageState extends State<ServicePage>
           body: SafeArea(
             child: Align(
               alignment: Alignment.topCenter,
-              child: Stack(
-                alignment: Alignment.center,
+              child: ListView(
                 children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.02),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Row(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Image.asset(
-                                  'images/BackButton.png',
-                                  height: 40,
-                                ),
-                              ),
-                              SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.2),
-                              const Text(
-                                'Services',
-                                style: TextStyle(
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20.0,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              const Spacer(),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.05),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 12.0, horizontal: 20.0),
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(25.0),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black26,
-                                  spreadRadius: 2,
-                                  blurRadius: 10,
-                                ),
-                              ],
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "Payment Services",
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    fontFamily: 'Inter',
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18.0,
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Row(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Image.asset(
+                                      'images/BackButton.png',
+                                      height: 40,
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: const Divider(
-                                    color: Colors.grey,
-                                    height: 20,
+                                  SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.2),
+                                  const Text(
+                                    'Services',
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0,
+                                      color: Colors.black,
+                                    ),
                                   ),
-                                ),
-                                services('images/Airtime.png', 'Airtime'),
-                              ],
+                                  const Spacer(),
+                                ],
+                              ),
                             ),
-                          ),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.05),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 20.0),
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(25.0),
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      spreadRadius: 2,
+                                      blurRadius: 10,
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Payment Services",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontFamily: 'Inter',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: const Divider(
+                                        color: Colors.grey,
+                                        height: 20,
+                                      ),
+                                    ),
+                                    Wrap(
+                                      runSpacing: 10.0,
+                                      children: [
+                                        services(
+                                            'images/Airtime.png', 'Airtime'),
+                                        services('images/Data.png', 'Data'),
+                                        services(
+                                            'images/CableTV.png', 'Cable TV'),
+                                        services(
+                                            'images/ExamPins.png', 'Exam Pins'),
+                                        services(
+                                            'images/DataPins.png', 'Data Pin'),
+                                        services('images/Electricity.png',
+                                            'Electricity'),
+                                        services('images/Betting.png',
+                                            'Fund Betting Wallet'),
+                                        services('images/Airtime2Cash.png',
+                                            'Airtime2cash'),
+                                        services(
+                                            'images/RechargeCardPrinting.png',
+                                            'Recharge card printing'),
+                                        services('images/ExamPins.png',
+                                            'Jamb E-PIN'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.05),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 20.0),
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(25.0),
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      spreadRadius: 2,
+                                      blurRadius: 10,
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Others",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontFamily: 'Inter',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: const Divider(
+                                        color: Colors.grey,
+                                        height: 20,
+                                      ),
+                                    ),
+                                    Wrap(
+                                      runSpacing: 10.0,
+                                      children: [
+                                        services(
+                                            'images/Pricing.png', 'Pricing'),
+                                        services(
+                                            'images/History.png', 'History'),
+                                        services('images/Notification.png',
+                                            'Notification'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -121,7 +210,7 @@ class _ServicePageState extends State<ServicePage>
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Container(
-        width: MediaQuery.of(context).size.width,
+        width: 100,
         padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
         decoration: const BoxDecoration(
           color: Color(0xFFF7FFF1),
@@ -138,6 +227,7 @@ class _ServicePageState extends State<ServicePage>
             SizedBox(width: MediaQuery.of(context).size.width * 0.03),
             Text(
               content,
+              textAlign: TextAlign.center,
               style: const TextStyle(
                 fontFamily: 'Inter',
                 fontWeight: FontWeight.bold,
